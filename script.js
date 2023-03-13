60adf0706bdedac943d41198b9f6ae181d5d10ad
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-14 03:00:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-function cipherRot13(str) {
-  str = str.toUpperCase();
-  return str.replace(/[A-Z]/g, rot13);
+function isCapLetter(num) {
+  if (num >= 65 && num <= 90) {
+    return true;
+  }
+  return false;
 
-  function rot13(correspondance) {
-    const charCode = correspondance.charCodeAt();
-    //A = 65, Z = 90
-    return String.fromCharCode(
-            ((charCode + 13) <= 90) ? charCode + 13
-                                    : (charCode + 13) % 90 + 64
-           );
-    
+}
+
+function range(num, min, max) {
+  if (num >= min && num <= max) {
+    return num;
+  } else {
+    return num - max + min - 1;
   }
 }
+
+//102 ---> 76
+range(100, 65, 90);
+
+//65-90
+
+function rot13(str) { // LBH QVQ VG!
+
+  str = str.toUpperCase();
+  var n = 0;
+  var l = "";
+  var str2 = "";
+
+  for (x = 0; x < str.length; x++) {
+    n = str.charCodeAt(x);
+    if (isCapLetter(n)) {
+      n = range(n + 13, 65, 90);
+    }
+    l = String.fromCharCode(n);
+    str2 += l;
+  }
+  return str2;
+}
+
+// Change the inputs below to test
+rot13("serR PBQR PNZC");
