ada44c3449588b3e9eaebdc63f7c1eee886bd1f7
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-13 11:50:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your Script here.
+function rot13(str) {
+var arr = [];
+  for(var i = 0; i < str.length; i++){
+    var code = 0;
+    code = str.charCodeAt(i);
+    if(code < 65){
+      arr.push(String.fromCharCode(code));
+    } else{
+      code = code + 13;
+      if(code > 90){
+      code = code % 90 + 65 -1;
+      arr.push(String.fromCharCode(code));
+    }
+      else{
+      arr.push(String.fromCharCode(code));
+    }
+   }
+}
+  return arr.join("");
+}
