9aeb69c9cfaf9782c5f65c3918b972600db9d657
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-18 16:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -46,8 +46,8 @@ function rot13(encodedStr) {
   return decodedArr.join(" ");
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+
 
 module.exports = rot13;
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
